2b73029db2f976aca6f4cca5e497efe86e7b17bd
freol35241 <freol@outlook.com>
2019-04-14 19:33:45 +0200

    Adding vectors to keep track of insertion order. This makes it possible to remove state_to_der_maps in the system spec.

--- pysim/cppsource/Variable_p.hpp
+++ pysim/cppsource/Variable_p.hpp
@@ -15,8 +15,11 @@ namespace pysim {
 
 struct VariablePrivate{
     std::map<std::string, double* > scalars;
+    std::vector<std::string> scalarNames;
     std::map<std::string, pysim::vector* > vectors;
+    std::vector<std::string> vectorNames;
     std::map<std::string, pysim::matrix* > matrices;
+    std::vector<std::string> matrixNames;
     std::map<std::string, std::string> descriptions;
 };
 
