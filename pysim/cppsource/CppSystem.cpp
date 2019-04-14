1dc156572054089ee8dbca0bc107853c82661d9c
freol35241 <freol@outlook.com>
2019-04-14 19:38:30 +0200

    Removing state/der/input/output c++ api and updating MACROs

--- pysim/cppsource/CppSystem.cpp
+++ pysim/cppsource/CppSystem.cpp
@@ -28,53 +28,6 @@ CppSystem::~CppSystem() {
 //
 ///////////////////////////////////////
 
-void CppSystem::state(double* state, const char* stateName, double* der, const char* derName,const char* description) {
-
-    string stateNameString(stateName);
-    boost::algorithm::trim(stateNameString);
-    states.addScalar(stateNameString, state, description);
-
-    string derNameString(derName);
-    boost::algorithm::trim(derNameString);
-    ders.addScalar(derNameString, der, description);
-
-    d_ptr->state_to_der_map_scalars[stateNameString] = derNameString;
-
-}
-
-void CppSystem::state(pysim::vector* state, const char* stateName, pysim::vector* der, const char* derName, const char* description) {
-
-    string stateNameString(stateName);
-    boost::algorithm::trim(stateNameString);
-    states.addVector(stateNameString, state, description);
-
-    string derNameString(derName);
-    boost::algorithm::trim(derNameString);
-    ders.addVector(derNameString, der, description);
-
-    d_ptr->state_to_der_map_vectors[stateNameString] = derNameString;
-}
-
-void CppSystem::state(pysim::matrix* state, const char* stateName, pysim::matrix* der, const char* derName, const char* description) {
-
-    string stateNameString(stateName);
-    boost::algorithm::trim(stateNameString);
-    states.addMatrix(stateNameString, state, description);
-
-    string derNameString(derName);
-    boost::algorithm::trim(derNameString);
-    ders.addMatrix(derNameString, der, description);
-
-    d_ptr->state_to_der_map_matrices[stateNameString] = derNameString;
-}
-
-
-void CppSystem::input(double* var, const char* name, const char* description) {
-    std::string str(name);
-    boost::algorithm::trim(str);
-    inputs.addScalar(str, var, description);
-}
-
 void CppSystem::par(std::vector<double>* var, const char* name, const char* description) {
     //TODO:: Make parameter
     string str(name);
@@ -90,19 +43,6 @@ void CppSystem::par(std::vector<std::vector<double>>* var, const char* name, con
     d_ptr->par_matrices[str] = var;
     d_ptr->par_descriptions[str] = string(description);
 }
-
-void CppSystem::input(pysim::vector* vars, const char* name, const char* description) {
-    string str(name);
-    boost::algorithm::trim(str);
-    inputs.addVector(str, vars, description);
-}
-
-void CppSystem::input(pysim::matrix* var, const char* name, const char* description) {
-    string str(name);
-    boost::algorithm::trim(str);
-    inputs.addMatrix(str, var, description);
-}
-
 
 void CppSystem::par(string* var, const char* name, const char* description) {
     string str(name);
@@ -123,24 +63,6 @@ void CppSystem::par(std::map<string, std::vector<double>>* var, const char* name
     boost::algorithm::trim(str);
     d_ptr->par_vector_maps[str] = var;
     d_ptr->par_descriptions[str] = string(description);
-}
-
-void CppSystem::output(double* var, const char* name, const char* description) {
-    string str(name);
-    boost::algorithm::trim(str);
-    outputs.addScalar(str, var, description);
-}
-
-void CppSystem::output(pysim::vector* vars, const char* name, const char* description) {
-    string str(name);
-    boost::algorithm::trim(str);
-    outputs.addVector(str, vars, description);
-}
-
-void CppSystem::output(pysim::matrix* var, const char* name, const char* description) {
-    string str(name);
-    boost::algorithm::trim(str);
-    outputs.addMatrix(str, var, description);
 }
 
 void CppSystem::setDiscrete(bool d) {
--- pysim/cppsource/CppSystem.hpp
+++ pysim/cppsource/CppSystem.hpp
@@ -38,30 +38,16 @@ protected:
     void par(std::string*, const char* name, const char* description);
     void par(std::map<std::string, double>*, const char* name, const char* description);
     void par(std::map<std::string, std::vector<double>>* var, const char* name, const char* description);
-    //void par(pysim::matrix* vars, const char* name, const char* description);
-
-    void input(double* var, const char* name, const char* desc);
-    void input(pysim::vector* vars, const char* name, const char* description);
-    void input(pysim::matrix*  vars, const char* name, const char* description);
-
-    void output(double* var, const char* name, const char* desc);
-    void output(pysim::vector* vars, const char* name, const char* desc);
-    void output(pysim::matrix* vars, const char* name, const char* desc);
-
-    void state(double* state, const char* stateName, double* der, const char* derName, const char* desc = "");
-    void state(pysim::vector* state, const char* stateName, pysim::vector* der, const char* derName, const char* desc = "");
-    void state(pysim::matrix* state, const char* stateName, pysim::matrix* der, const char* derName, const char* desc = "");
-
 
     void setNextUpdate(double t);
     void setDiscrete(bool d = true);
 
 };
 
-#define STATE(x,dx,s) state(&x,#x,&dx,#dx,s);
+#define STATE(x,dx,s) { states.add(#x,&x,s); ders.add(#dx,&dx,s); };
 
-#define INPUT(x,s) input(&x,#x,s);
-#define OUTPUT(x,s) output(&x,#x,s);
+#define INPUT(x,s) inputs.add(#x,&x,s);
+#define OUTPUT(x,s) outputs.add(#x,&x,s);
 
 #define PAR(x,s) par(&x,#x,s);
 }
